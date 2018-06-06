62e859601f45940a471a3ad21e73fedca29180d4
imamitsehgal@gmail.com <Indian!123>
2018-06-06 16:25:31 +0530

    Added show and describe command. Changed to HiveSQLException

--- src/main/scala/org/apache/spark/sql/hive/thriftserver/SQLValidator.scala
+++ src/main/scala/org/apache/spark/sql/hive/thriftserver/SQLValidator.scala
@@ -1,13 +1,14 @@
 package org.apache.spark.sql.hive.thriftserver
 
+import org.apache.hive.service.cli.{HiveSQLException, OperationState, OperationStatus}
+
 object SQLValidator {
 
   def validate(user:String,sql:String):String={
-    if(sql.trim.toLowerCase.startsWith("select")){
-      return sql;
-    }else{
-      throw new RuntimeException(s"User $user is not allowed to run this query. You are only allowed to run a select query")
-    }
+     sql.trim.split(" ")(0) match {
+       case "select" | "describe" | "show" => sql;
+       case _ => throw new HiveSQLException(s"User $user is not allowed to run this query. Only SELECT queries are permitted","NOT_PERMITTED_QUERY",403)
+     }
 
   }
 }
